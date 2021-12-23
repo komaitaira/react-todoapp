213226350467dc6111fec2d688072e30f17bc27f
komaitaira <komaitaira@gmail.com>
2021-12-24 00:38:08 +0900

    importパス修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Button } from './components/atoms/button';
+import { Button } from './components/atoms/Button';
 
 function App() {
   return (
